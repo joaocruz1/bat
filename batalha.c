d32ddcf3a91df6badde8bc67b29dfc9fb832faeb
Joao Cruz <joaovcruz50@gmail.com>
2024-11-28 15:17:03 -0300

    commit_02

--- batalha.c
+++ batalha.c
@@ -5,24 +5,15 @@
 #define TAMANHOCAMP 5
 #define TAMANHONICK 50
 
-
-int camp0(int camp1[][TAMANHOCAMP],int camp2[][TAMANHOCAMP]){
-    for(int i = 0; i<5; i++){
-        for(int j = 0; j<5; j++){
-            camp1[i][j] = 0;
-            camp2[i][j]= 0;
-        }
-    }
-}
-
 char ler_e_armazenar_nick(char nick[],int tamanho, int player){
 
     while(1){
     printf("Adicone seu apelido Player %d \n", player); 
-    fgets(nick, tamanho, stdin); // fgets adiciona no final da string o ENTER (\n)
+    fgets(nick, tamanho, stdin); 
 
     int posicao_enter = strcspn(nick, "\n");
     nick[posicao_enter] = '\0';
+    fflush(stdin);
 
     if (strlen(nick)>0 )
     {
@@ -43,37 +34,123 @@ int preenchercamp(int camp[][TAMANHOCAMP],char player[]){
         int barco[2];
 
         printf("%s; Escolha a posicao da coluna para seu barco %d \n", player, i);
-        scanf("%d", &barco[0]-1);
+        scanf("%d", &barco[0]);
+        barco[0]--;
         printf("%s; Escolha a posicao da linha para seu barco %d \n", player, i);
-        scanf("%d", &barco[1]-1);
+        scanf("%d", &barco[1]);
+        barco[1]--;
         printf("%s; A pontuacao que o barco %d vale eh \"%d\" \n", player, i, pont_barco);
         
-        if(barco[0] < 6 && barco[1] <5 && barco[0]> 0 && barco[1]> 0 ){
-
-            if(camp[barco[0]][barco[1]] != 0){
+        if (barco[0] >= 0 && barco[0] < TAMANHOCAMP && barco[1] >= 0 && barco[1] < TAMANHOCAMP) {
+            if (camp[barco[0] ][barco[1] ] != 0) {  
                 printf("Esse espaco ja contem um barco \n");
                 i--;
-
-            }else{
-                camp[barco[0]][barco[1]] = pont_barco;
+            } else {
+                camp[barco[0]][barco[1]] = pont_barco;  
             }
-        }else{
-            printf("Valor de posicao invalido(deve ser de 1 a 5)");
+        } else {
+            printf("Valor de posicao invalido (deve ser de 1 a 5)\n");
             i--;
-            
         }
+
     }
 
 
 }
+
+void imprimir_placar(int camp[][TAMANHOCAMP],char player[]){
+
+}
+
+int imprimir_camp(int camp[][TAMANHOCAMP], char player[]){
+
+    printf("Campo de %s :\n", player);
+    for(int i = 0; i < TAMANHOCAMP; i++){
+        for(int j = 0; j < TAMANHOCAMP; j++){
+            printf("%d ", camp[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define TAMANHOCAMP 5
+#define TAMANHONICK 50
+
+void jogar(int camp1[][TAMANHOCAMP], int camp2[][TAMANHOCAMP], char player1[], char player2[]) {
+    int acertou_barco_player1 = 0;
+    int acertou_barco_player2 = 0;
+    char vitorioso[TAMANHONICK] = "";
+    int jogadas = 0;
+
+    while (strlen(vitorioso) == 0 && jogadas < 9) {
+
+        // Turno do Player 1
+        int pos_ataque[2];
+        printf("%s, escolha a coluna para atacar: ", player1);
+        scanf("%d", &pos_ataque[0]);
+        printf("%s, escolha a linha para atacar: ", player1);
+        scanf("%d", &pos_ataque[1]);
+
+        // Validação das posições
+        if (pos_ataque[0] >= 0 && pos_ataque[0] < TAMANHOCAMP && pos_ataque[1] >= 0 && pos_ataque[1] < TAMANHOCAMP) {
+            
+            if (camp2[pos_ataque[0]][pos_ataque[1]] != 0) {
+                printf("Acertou!\n");
+                acertou_barco_player1++;
+                camp2[pos_ataque[0]][pos_ataque[1]] = 0; 
+                
+                if (acertou_barco_player1 == 4) {
+                    strcpy(vitorioso, player1);
+                    break;
+                }
+            } else {
+                printf("Você errou!\n");
+            }
+            jogadas++;
+        }
+
+        // Turno do Player 2 (semelhante ao turno do Player 1)
+        printf("%s, escolha a coluna para atacar: ", player2);
+        scanf("%d", &pos_ataque[0]);
+        printf("%s, escolha a linha para atacar: ", player2);
+        scanf("%d", &pos_ataque[1]);
+
+        if (pos_ataque[0] >= 0 && pos_ataque[0] < TAMANHOCAMP && pos_ataque[1] >= 0 && pos_ataque[1] < TAMANHOCAMP) {
+            
+            if (camp1[pos_ataque[0]][pos_ataque[1]] != 0) {
+                printf("Acertou!\n");
+                acertou_barco_player2++;
+                camp1[pos_ataque[0]][pos_ataque[1]] = 0;
+
+                if (acertou_barco_player2 == 4) {
+                    strcpy(vitorioso, player2);
+                    break;
+                }
+            } else {
+                printf("Você errou!\n");
+            }
+            jogadas++;
+        }
+    }
+
+    // Exibição do resultado final
+    if (strlen(vitorioso) > 0) {
+        printf("Parabéns, %s venceu o jogo!\n", vitorioso);
+    } else {
+        printf("Jogo empatado!\n");
+    }
+}
+
+
+
 int main(){
 
-    int jogadas = 0;
-    int vitorioso = 0;
-
-    int camp1[TAMANHOCAMP][TAMANHOCAMP];
-    int camp2[TAMANHOCAMP][TAMANHOCAMP];
-    camp0(camp1,camp2);
+    int camp1[TAMANHOCAMP][TAMANHOCAMP] = {0}; 
+    int camp2[TAMANHOCAMP][TAMANHOCAMP] = {0};
 
     char player1[TAMANHONICK];
     ler_e_armazenar_nick(player1,TAMANHONICK, 1);
@@ -86,12 +163,12 @@ int main(){
     preenchercamp(camp1,player1);
     preenchercamp(camp2,player2);
 
-    printf("Inciando o jogo !!!!!!!!!!");
+    printf("Inciando o jogo !!!!!!!!!! \n");
 
-    while(vitorioso == 0 || jogadas <8){
-        
-    }
+    imprimir_camp(camp1,player1);
+    imprimir_camp(camp2,player2);
 
+    jogar(camp1,camp2,player1,player2);
     
 }
 
